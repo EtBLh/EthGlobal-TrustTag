c89add959d1c0e040146f4304a1a8fb7e7c9be05
etblh <hugo432121@gmail.com>
2025-04-04 17:03:28 +0000

    added shadcn

--- world-miniapp/src/App.tsx
+++ world-miniapp/src/App.tsx
@@ -3,7 +3,15 @@
 
 export default function App() {
   return (
-    <main className="flex min-h-screen flex-col ">
+    <main className="flex min-h-screen flex-col bg-black p-1">
+      <header className='text-lg font-bold flex flex-row gap-1/2 items-baseline'>
+        {/* <Typography variant="heading" className='text-white'>TrustTag</Typography> */}
+        {/* <Typography variant="mono" className='text-white text-xl'>TAG</Typography>
+        <Typography variant="number" className='text-white'>12.32</Typography> */}
+      </header>
+      <div className='text-xl'>
+        
+      </div>
       {/* <VerifyBlock />
       <PayBlock /> */}
     </main>
