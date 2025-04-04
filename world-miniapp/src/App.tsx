7a9bf4a80e421ef793bbdb60eea5ea411adaae28
etblh <hugo432121@gmail.com>
2025-04-04 16:17:36 +0000

    pack world-miniapp into a directory

---
+++ world-miniapp/src/App.tsx
@@ -0,0 +1,11 @@
+// import { PayBlock } from "./components/Pay";
+// import { VerifyBlock } from "./components/Verify";
+
+export default function App() {
+  return (
+    <main className="flex min-h-screen flex-col ">
+      {/* <VerifyBlock />
+      <PayBlock /> */}
+    </main>
+  );
+}
